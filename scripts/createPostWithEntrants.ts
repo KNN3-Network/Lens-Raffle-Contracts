caf414114999aba9eef5ebbd3bc7a1efe4f8b154
Brentably <Brent@pabnet.com>
2022-12-11 15:44:18 -0500

    I had to change up the configuration to get typescript to start registering properly

--- scripts/createPostWithEntrants.ts
+++ scripts/createPostWithEntrants.ts
@@ -2,12 +2,15 @@ import { ethers } from "hardhat";
 import LENS_HUB_ABI from '../abi/LensHubABI.json';
 import { DataTypes } from "../LensTypes/LensHub";
 import { client, challenge, authenticate } from '../lensApi/api'
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import getToken from './helpers/authenticate'
+
 const LENS_HUB_MUMBAI_PROXY = "0x60Ae865ee4C725cd04353b5AAb364553f56ceF82"
 
 
 //the purpose of this script is to create a post and have the other signers comment. 
 async function main() {
-  const deployers = await ethers.getSigners()
+  const deployers:SignerWithAddress[] = await ethers.getSigners()
   // array of 5 signers
   const contracts = await Promise.all(deployers.map(async dep => await ethers.getContractAt(LENS_HUB_ABI, LENS_HUB_MUMBAI_PROXY, dep)))
   console.log(contracts.length)
@@ -15,26 +18,8 @@ async function main() {
 
 // dep0 makes a post, dep1, dep2, dep3 comment. 
   
-
-try {
-  const address = deployers[0].address
-  /* first request the challenge from the API server */
-  const challengeInfo = await client.query(challenge, { address } ).toPromise()
-  // const provider = new ethers.providers.Web3Provider(window.ethereum);
-  // const signer = provider.getSigner()
-  const signer = deployers[0]
-  console.log(challengeInfo)
-  /* ask the user to sign a message with the challenge info returned from the server */
-  const signature = await signer.signMessage(challengeInfo.data.challenge.text)
-  /* authenticate the user */
-  const authData = await client.mutation(authenticate, {address, signature}).toPromise()
-  /* if user authentication is successful, you will receive an accessToken and refreshToken */
-  const { data: { authenticate: { accessToken }}} = authData
-  console.log({ accessToken })
-} catch (err) {
-  console.log('Error signing in: ', err)
-}
-   
+const token = await getToken(deployers[0])
+   console.log(token)
 
 
 
--- scripts/helpers/authenticate.ts
+++ scripts/helpers/authenticate.ts
@@ -1,5 +1,30 @@
-// import { client, challenge, authenticate } from '../../lensApi/api'
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
+import { Signer } from 'ethers/lib/ethers'
+import { client, challenge, authenticate } from '../../lensApi/api'
 
-// export default const Authenticate = () => {
 
-// }
+export default async (deployer: SignerWithAddress) => {
+
+
+  try {
+    const address = deployer.address
+    /* first request the challenge from the API server */
+    const challengeInfo = await client.query(challenge, { address } ).toPromise()
+    // const provider = new ethers.providers.Web3Provider(window.ethereum);
+    // const signer = provider.getSigner()
+    const signer = deployer
+    console.log(challengeInfo)
+    /* ask the user to sign a message with the challenge info returned from the server */
+    const signature = await signer.signMessage(challengeInfo.data.challenge.text)
+    /* authenticate the user */
+    const authData = await client.mutation(authenticate, {address, signature}).toPromise()
+    /* if user authentication is successful, you will receive an accessToken and refreshToken */
+    const { data: { authenticate: { accessToken }}} = authData
+    console.log({ accessToken })
+    return accessToken
+  } catch (err) {
+    console.log('Error signing in: ', err)
+  }
+
+
+}
